b8342476f594e246e1b304345dea45df66e78e9a
Huan <zixia@zixia.net>
2021-01-04 16:01:59 +0000

    fix port map

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,9 +6,10 @@ set -o pipefail
 docker run \
   --rm \
   -ti \
-  -p 2222:22 \
-  -p 2323:23 \
-  -p 8080:80 \
+  -p 22:2222 \
+  -p 23:2323 \
+  -p 80:8080 \
+  -v /data/bbs:/bbs \
   -e VIRTUAL_HOST=bbs.zixia.net,*.bbs.zixia.net \
   -e LETSENCRYPT_HOST=bbs.zixia.net \
   -e HTTPS_METHOD=noredirect \
