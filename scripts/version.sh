94a6094ec78892c14fcfa76a2fdbacfcd9048e05
Huan <zixia@zixia.net>
2021-01-24 10:37:55 +0800

    use vX.Y.Z for commit message

--- scripts/version.sh
+++ scripts/version.sh
@@ -6,6 +6,6 @@ set -o pipefail
 newVersion=$(awk -F. '{print $1"."$2"."$3+1}' < VERSION)
 echo ${newVersion} > VERSION
 git add VERSION
-git commit -m "${newVersion}"
+git commit -m "v${newVersion}"
 git tag "v${newVersion}"
-echo "Bumped version to ${newVersion}"
+echo "Bumped version to v${newVersion}"
