865462db898dec88fac7aa15a9bf65995908aeb2
Huan <zixia@zixia.net>
2021-01-25 02:26:53 +0800

    follow npm version patch rule: do not use v in ci message

--- scripts/version.sh
+++ scripts/version.sh
@@ -6,6 +6,6 @@ set -o pipefail
 newVersion=$(awk -F. '{print $1"."$2"."$3+1}' < VERSION)
 echo ${newVersion} > VERSION
 git add VERSION
-git commit -m "v${newVersion}"
-git tag "v${newVersion}"
-echo "Bumped version to v${newVersion}"
+git commit -m "${newVersion}"
+git tag "${newVersion}"
+echo "Bumped version to ${newVersion}"
