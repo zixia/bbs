404c3f1ebf9d314d3d34defe7bf23544777363d4
Huan <zixia@zixia.net>
2021-01-25 14:27:40 +0800

    clean docker script

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,20 +3,30 @@
 set -e
 set -o pipefail
 
-docker run \
-  --name bbs.zixia.net \
-  --rm \
-  -ti \
+# https://github.com/zixia/bbs.zixia.net/issues/12
+GDB_OPTIONS=\
   --cap-add=SYS_PTRACE \
   --security-opt apparmor=unconfined \
+
+PORT_OPTIONS=\
   -p 2222:22 \
   -p 22222:2222 \
   -p 2323:23 \
   -p 8080:80 \
-  -v /data/bbs:/bbs \
+
+ENV_OPTIONS=\
   -e VIRTUAL_HOST=bbs.zixia.net,*.bbs.zixia.net,wforum.zixia.net,*.wforum.zixia.net \
   -e LETSENCRYPT_HOST=bbs.zixia.net,wforum.zixia.net \
   -e HTTPS_METHOD=noredirect \
+
+docker run \
+  --rm \
+  -ti \
+  $GDB_OPTIONS \
+  $PORT_OPTIONS \
+  $ENV_OPTIONS \
+  --name bbs.zixia.net \
+  -v /data/bbs:/bbs \
   ghcr.io/zixia/bbs.zixia.net
   # --entrypoint bash \
   # bbs.zixia.net
